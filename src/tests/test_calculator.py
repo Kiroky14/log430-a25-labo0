5b7b5746022ea4b241afb827c2c5028f743972dd
Kylian Pasquereau <120525908+Kiroky14@users.noreply.github.com>
2025-09-10 21:23:41 -0400

    fix : chemin d'accès pour accéder au module calculator

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -4,10 +4,18 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0"
 
+
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
 # TODO: ajoutez les tests
